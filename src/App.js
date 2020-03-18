7bf9084f117e00a16cdf38a5743d3831c47bddc1
unknown <ixentrik@gmail.com>
2020-03-18 11:36:26 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>BEST IN SLOT MANAGER</h1>
         <Router>
           <Route exact path={"/"}>
             <div> <MainPage
